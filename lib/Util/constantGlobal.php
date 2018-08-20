205bf55c3f6b42cc01173ebb58ea400bfea6feec
Andres Gonzalez <andresg9108@gmail.com>
2018-08-19 19:54:51 -0500

    Guardando cambios

--- lib/Util/constantGlobal.php
+++ lib/Util/constantGlobal.php
@@ -13,7 +13,6 @@ class constantGlobal extends constant{
 
     // EMAIL
     const EMAIL_CHECKIN_URL = "http://localhost/jsonphp/testHTML/validateUser/?id=<1?>&code=<2?>";
-    // const EMAIL_CHECKIN_URL = "http://andresgonzalez.me/example/testHTML/validateUser/?id=<1?>&code=<2?>";
     const EMAIL_CHECKIN_SUBJECT = "Thank you for registering";
     const EMAIL_CHECKIN_MESSAGE = "<1?>";
 
