3da88fc405e72af92b65500b38e813197df2ac72
Andres Gonzalez <andresg9108@gmail.com>
2018-08-20 08:22:36 -0500

    Comenzando con validacion de email.

--- administration/view/user/load.php
+++ administration/view/user/load.php
@@ -9,6 +9,9 @@ $aResponse = Util::getResponseArray(false, (object)[]
 	,'', constantGlobal::ERROR_404);
 
 switch ($sAction) {
+	case 'validateEmailByCode':
+		$aResponse = $oUserController->validateEmailByCodeAction((object)$_GET, (object)$_POST);
+		break;
 	case 'validateSession':
 		$aResponse = $oUserController->validateSessionAction((object)$_GET, (object)$_POST);
 		break;
--- testHTML/js/validateUser.js
+++ testHTML/js/validateUser.js
@@ -7,7 +7,26 @@ $(function(){
 function setView(){
 	let iId = getParameterBysName('id');
 	let sCode = getParameterBysName('code');
-	
-	console.log(iId);
-	console.log(sCode);
+
+	let oDatos = {};
+    let sUrl = 'administration/publicData/appRegistration';
+    $.when($.post(g_sBackEnd+sUrl, oDatos))
+    .then(function(oResponse){
+        let aResponse = oResponse.response;
+        let iId = aResponse.id;
+        let sRegCod = aResponse.registration_code;
+        sRegCod = getDecodeRegCod(sRegCod);
+
+        let oDatos2 = {
+            'id': iId,
+            'registration_code': sRegCod
+        };
+        let sUrl2 = 'administration/user/validateEmailByCode';
+        $.when($.post(g_sBackEnd+sUrl2, oDatos2))
+        .then(function(oResponse){
+        	console.log(oResponse);
+        })
+        .fail(function(){});
+    })
+    .fail(function(){});
 }
