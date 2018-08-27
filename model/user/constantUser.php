489245f8063b4a3a4ba925cf6b8a6ce83894136d
Andres Gonzalez <andresg9108@gmail.com>
2018-08-27 14:37:40 -0500

    Agregando constantes de user en ingles

--- model/user/constantUser.php
+++ model/user/constantUser.php
@@ -6,14 +6,14 @@ use lib\MVC\constant;
 
 class constantUser extends constant{
 
-	const VAL_EMPTY_EMAIL = "Debes enviar un email.";
-	const VAL_EMPTY_USUARIO = "Debes enviar un usuario.";
-	const VAL_EMPTY_PASSWORD = "Debes enviar una contraseña.";
-	const VAL_VAL_EMAIL = "Debes enviar un email valido.";
-	const VAL_VAL_USERNAME = "Debes enviar un usuario valido e incluir al menos 5 caracteres. Caracteres permitidos: a, b, c... 0, 1, 2, 3... guion (-), guion bajo (_) y punto (.). No incluyas acentos del español (Ej: á), la letra ñ, ni espacios.";
-	const VAL_VAL_PASSWORD = "La contraseña se debe enviar con al menos 5 caracteres.";
-	const VAL_EXISTING_EMAIL = "Ya se registró alguien con este email ('<1?>'). Si este es tu email, ve a la sección recuperar contraseña.";
-	const VAL_EXISTING_USERNAME = "Debes enviar un usuario diferente, ya se a registrado alguien con: <1?>";
+	const VAL_EMPTY_EMAIL = "You must send an email.";
+	const VAL_EMPTY_USUARIO = "You must send a user.";
+	const VAL_EMPTY_PASSWORD = "You must send a password.";
+	const VAL_VAL_EMAIL = "You must send a valid email.";
+	const VAL_VAL_USERNAME = "You must send a valid user and include at least 5 characters. Permitted characters: a, b, c ... 0, 1, 2, 3 ... hyphen (-), underscore (_) and period (.). Do not include accents of Spanish (Ej: á), the letter ñ, or spaces.";
+	const VAL_VAL_PASSWORD = "The password must be sent with at least 5 characters.";
+	const VAL_EXISTING_EMAIL = "Someone has already registered with this email ('<1?>'). If this is your email, go to the recover password section.";
+	const VAL_EXISTING_USERNAME = "You must send a different user, someone has already been registered with: <1?>";
 
 	// Spanish
 	const SPAN_VAL_EMPTY_EMAIL = "Debes enviar un email.";
