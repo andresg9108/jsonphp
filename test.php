1cdedca4453d6417e79fb39f3f248d3ecd87ef90
Andres Gonzalez <andresg9108@gmail.com>
2019-07-16 13:07:02 -0500

    Test of connectiondb package composer

--- test.php
+++ test.php
@@ -6,44 +6,23 @@ ini_set('display_errors', '1');
 const __DIRMAIN__ = "./";
 require_once __DIRMAIN__.'autoload.php';
 
-use lib\Useful\{Useful, constantGlobal};
-use model\{connection, systemException};
-use model\person\{person, constantPerson};
-use model\user\userProxy;
+use andresg9108\connectiondb\connection;
 
-/*$oConnection = connection::getInstance();
+$aConnection = [
+'motor' => 'mysql',
+'server' => 'localhost',
+'user' => 'root',
+'password' => '',
+'database' => 'my_database'
+];
+
+$oAConnection = (object)$aConnection;
+
+$oConnection = connection::getInstance($oAConnection);
 $oConnection->connect();
 
-$oPerson = person::getInstance($oConnection);
-$oPerson->iId = 1;
-$oPerson->sName = 'Andres';
-$oPerson->sLastName = 'Gonzalez';
-$oPerson->save();
+$oConnection->queryArray("SELECT * FROM user;", ['field1', 'field2', 'field3', 'field4']);
+$aResponse = $oConnection->getQuery();
 
-$oConnection->commit();
-$oConnection->close();*/
-
-/*$oConnection = connection::getInstance();
-$oConnection->connect();
-
-$aData = [];
-$aData['id'] = 100;
-$aData['profile'] = 200;
-$oData = (object)$aData;
-$sCode = Useful::getJWT($oData, $oConnection);
-
-echo $sCode;*/
-
-/*$sCode = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NjEwNTIyNDMsImV4cCI6MTU2MTEzODY0MywiY29kZSI6eyJpZCI6MTAwLCJwcm9maWxlIjoyMDB9fQ.nqwsxlcPLIH-ViVQwX4sEoT4nKyJH_VS2jIL2qGZpOs";
-$oObject = Useful::getDecodeJWT($sCode);
-
-echo json_encode($oObject);*/
-
-$aUser = [];
-$aUser['user'] = 'andres';
-$aUser['password'] = '1234567890';
-$oUser = (object)$aUser;
-$oResponse = userProxy::validatelogIn($oUser);
-
-echo json_encode($oResponse);
+echo json_encode($aResponse);
 
